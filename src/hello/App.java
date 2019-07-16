12c458245b89b5bc98f02644b3c0fe37717de742
olatundi <isaacola2121@gmail.com>
2019-07-16 11:25:23 +0100

    first feature

--- src/hello/App.java
+++ src/hello/App.java
@@ -1,10 +1,22 @@
 package hello;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 	}
-
+	
+	private Map <String,Integer> jaMap= new HashMap<String,Integer>();
+	
+	public void addItem(String key, Integer value) {
+		this.jaMap.put(key, value);
+	}
+	
+	public void displayItem(String key) {
+		System.out.println("The value of "+key+" is: "+ String.valueOf(this.jaMap.get(key)));	
+	}
 }
